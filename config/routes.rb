23a21a47a048d711b7bf9cb2ee0fc661af664fdd
SGhyun <sghiroo@sogang.ac.kr>
2017-09-22 12:11:40 +0000

    기본으로

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   
   #기본
-  # get '/keyboard' => 'basic#keyboard_init'
-  # post '/message' => 'basic#chat_control'
+  get '/keyboard' => 'basic#keyboard_init'
+  post '/message' => 'basic#chat_control'
   
   # 뉴스 검색 기능
   # get '/keyboard' => 'news#keyboard_init'
   # post '/message' => 'news#chat_control'
   
   # 회원 가입 기능
-  get '/keyboard' => 'users#keyboard_init'
-  post '/message' => 'users#chat_control'
+  # get '/keyboard' => 'users#keyboard_init'
+  # post '/message' => 'users#chat_control'
 end
